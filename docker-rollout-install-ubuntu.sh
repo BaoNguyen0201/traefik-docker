fb71268f72a48f9c4740e4bb11f62256007bea64
root <root@ip-10-0-29-160.ap-southeast-1.compute.internal>
2024-08-22 09:42:43 +0000

    add readme

---
+++ docker-rollout-install-ubuntu.sh
@@ -0,0 +1,8 @@
+# Create directory for Docker cli plugins
+mkdir -p ~/.docker/cli-plugins
+
+# Download docker-rollout script to Docker cli plugins directory
+curl https://raw.githubusercontent.com/wowu/docker-rollout/master/docker-rollout -o ~/.docker/cli-plugins/docker-rollout
+
+# Make the script executable
+chmod +x ~/.docker/cli-plugins/docker-rollout
